15a53fc2c5b3edbb355f178ae9bdf1508477682e
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-10-25 22:56:13 -0300

    primer entrega jvs

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log ("hola coderhouse")
+
+let nombreProducto1 = "arnes con cabo de vida" 
+console.log (nombreProducto1);
+
+let nombreProducto2 = "jafas de seguridad"
+console.log (nombreProducto2);
+
+let nombreProducto3 = "mascara de doble filtro"
+console.log (nombreProducto3);
+
+let nombreUsuario = "";
+
+console.log (nombreUsuario)
+
+let bienvenidaUsuario = prompt ("Todo lo que buscas esta aca!!!");
+
+console.log (nombreUsuario + prompt );
