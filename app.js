45bad89f3b0fa91d00fd63f1caa1c7cc28f97e9d
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-10-31 19:51:00 -0300

    primera entrega jvs

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-let nombreProducto1 = "arnes con cabo de vida" 
+let nombreUsuario = prompt ("ingrese su nombre");
+let apellidoUsuario = prompt ("ingrese su apellido");
+let edadUsuario = prompt ("ingrese su edad");
+
+let valorVerdadero = true 
+let valorNegativo = false 
+
+if ( edadUsuario >= 18 ) {
+
+alert ("todo lo que buscas esta aca"+ " " + nombreUsuario + " " + apellidoUsuario + "!!!");
+}
+
+if (edadUsuario < 18) {
+    
+}
+
+console.log ("hola" +" " + nombreUsuario + " " + apellidoUsuario );
+
+
+
+
+let nombreProducto1 = "arnes con cabo de vida";
 console.log (nombreProducto1);
 
-let nombreProducto2 = "jafas de seguridad"
+let nombreProducto2 = "jafas de seguridad";
 console.log (nombreProducto2);
 
-let nombreProducto3 = "mascara de doble filtro"
+let nombreProducto3 = "mascara de doble filtro";
 console.log (nombreProducto3);
 
-let bienvenidaUsuario = prompt ("Todo lo que buscas esta aca!!!");
 
 
