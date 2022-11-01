4b4aa20e8d9b75c2415b0760026842d2bec70244
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-10-31 22:34:12 -0300

    primer entrega de proyecto jvs

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-let nombreUsuario = prompt ("ingrese su nombre");
+function saludar () {
+console.log ("hola" +" " + nombreUsuario + " " + apellidoUsuario );     
+}
+function mostrarMensaje () {
+    alert ("no puede ingresar a este sitio, intentelo nuevamente!!!");
+}
+
+let nombreUsuario = prompt ("ingrese su nombre");  
 let apellidoUsuario = prompt ("ingrese su apellido");
 let edadUsuario = prompt ("ingrese su edad");
 
-let valorVerdadero = true 
-let valorNegativo = false 
 
 if ( edadUsuario >= 18 ) {
-
 alert ("todo lo que buscas esta aca"+ " " + nombreUsuario + " " + apellidoUsuario + "!!!");
-
-console.log ("hola" +" " + nombreUsuario + " " + apellidoUsuario );}
+saludar ()
+for (let i=0; i<3; i++) {
+    saludar ();
+}
+}
 else {
-    alert ("no puede ingresar a este sitio, intentelo nuevamente!!!");
+    mostrarMensaje ();
 }
 
 
