edb575ae71e9906d51f4f72d6e377211ac7fc89a
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-11-13 22:32:44 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let nombreUsuario = prompt ("ingrese su nombre");
+let apellidoUsuario = prompt ("ingrese su apellido");
+let edadUsuario = prompt ("ingrese su edad");
+
 function saludar () {
 console.log ("hola" +" " + nombreUsuario + " " + apellidoUsuario );     
 }
@@ -5,15 +9,11 @@ function mostrarMensaje () {
     alert ("no puede ingresar a este sitio, intentelo nuevamente!!!");
 }
 
-let nombreUsuario = prompt ("ingrese su nombre");  
-let apellidoUsuario = prompt ("ingrese su apellido");
-let edadUsuario = prompt ("ingrese su edad");
-
 
 if ( edadUsuario >= 18 ) {
 alert ("todo lo que buscas esta aca"+ " " + nombreUsuario + " " + apellidoUsuario + "!!!");
 saludar ()
-for (let i=0; i<3; i++) {
+for (let i=0; i<1; i++) {
     saludar ();
 }
 }
@@ -21,7 +21,27 @@ else {
     mostrarMensaje ();
 }
 
+let productoUno = {
+    producto:"arnes de seguridad",
+    marca: "iram",
+    modelo: "anticaida",
+    color: "amarillo",
+}
+let productoDos = {
+    producto:"gafas de seguridad",
+    marca:"deltaplus",
+    modelo:"ecoline" ,
+    color: "transparente",}
+let productotres = {
+    producto:"mascara con filtro",
+    marca:"stim",
+    modelo:"bifiltro",
+    color:"azul",
+}
 
+console.log (productoUno);
+console.log (productoDos);
+console.log (productotres);
 
 
 
