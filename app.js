838eb46b0ecfa617341061b3129b927b7bf8e965
Jorge Zayas <jorge_zayas1998@hotmail.com.ar>
2022-11-23 21:32:50 -0300

    3ra pre-entrega

--- app.js
+++ app.js
@@ -1,55 +1,141 @@
-let nombreUsuario = prompt ("ingrese su nombre");
-let apellidoUsuario = prompt ("ingrese su apellido"); 
-let edadUsuario = prompt ("ingrese su edad");
-
-while (edadUsuario == null || /\D/.test(edadUsuario) || edadUsuario == "") {
-    edadUsuario = prompt("ingrese su edad");
-};
-
-function saludar () {
-console.log ("hola" +" " + nombreUsuario + " " + apellidoUsuario );     
-}
-function mostrarMensaje () {
-    alert ("no puede ingresar a este sitio, intentelo nuevamente!!!");
-}
-
-if (edadUsuario >= 18 ) {
-alert ("todo lo que buscas esta aca"+ " " + nombreUsuario + " " + apellidoUsuario + "!!!");
-saludar ()
-
-}
-else {
-    mostrarMensaje ();
-
-}
-
-
-const proteccionDeAltura = [
-    {
-    producto:"arnes de seguridad",    
-    marca: "iram",
-    modelo: "anticaida",
-    color: "amarillo",
+const contenedorprincipal = document.getElementById("contenedor")
+const contenedorCarrito = document.getElementById("carritoHTML")
+const precioHTML = document.getElementById("totalprecio")
+let productos = []
+let carrito = []
+let PRODUCTOS = "/productos.json"
+Swal.fire({
+    title: 'Eres mayor de 18 años?',
+    showDenyButton: true,
+    showCancelButton: false,
+    confirmButtonText: 'SI', 
+    denyButtonText: `NO`,
+    background: "radial-gradient(yellow, orange )",
+    border: "15px",
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+        $.getJSON(PRODUCTOS, function (res, req) {
+            if (req === "success"){
+                let item = res; 
+                for(const producto of item){
+                    productos.push(producto);
+                    function renderproducto(){
+                        productos.forEach(E=>{
+                            let nuevoNodo = document.createElement("div");
+                            nuevoNodo.innerHTML = ` 
+                            <div>
+                            <img src="${E.img}" alt="producto 1">
+                            <div class="informacion">
+                                <p class="info">${E.producto}</p>
+                                <p class="precio">$ ${E.precio}</p>
+                                <p>${E.modelo}</p>
+                                <p>${E.color}</p>
+                                <button onclick="addToCart(${productos.indexOf(E)})">Comprar</button>
+                            </div>
+                        </div>
+                            `
+                            contenedorprincipal.appendChild(nuevoNodo)
+                        })
+                    }
+                }
+                renderproducto()
+            }
+        })
+    } else if (result.isDenied) {
+        let mensaje = document.createElement("div");
+        mensaje.innerHTML =`
+            <h1>Tenes que ser mayor de 18 años para poder comprar</h1>
+        `
+        contenedorprincipal.appendChild(mensaje)
     }
-]
+  })
 
-const proteccionOcular = [
-    {
-    producto:"gafas de seguridad",
-    marca:"deltaplus",
-    modelo:"ecoline" ,
-    color: "transparente",}]
+  function addToCart(index){
+    const producto = productos[index];
+    if (carrito.length > 0) {
+        var noExiste = true;
+        for (var i = 0; i < carrito.length; i++) {
+            if (producto.producto === carrito[i].producto) {
+                carrito[i].cantidad++;
+                noExiste = false;
+            }
+        }
+        if (noExiste) {
+            producto.cantidad =   1;
+            carrito.push(producto);
+        }
+    }
+    else {
+        producto.cantidad = 1;
+        carrito.push(producto);
+    }
+    let totalPrecio = producto.precio * producto.cantidad
+    // console.log(carrito);
+    renderCarrito ()
+    sumarPrecios()
+  }
+  function renderCarrito () {
+    contenedorCarrito.innerHTML = ''
+    if (carrito.length > 0){
+        carrito.forEach(prod => {
+            let Nuevoelemento = document.createElement('tbody') 
+            Nuevoelemento.innerHTML += `
+            <thead >
+                    <td>${prod.producto}</td>
 
-const proteccionRespiratoria = [
-    {
-    producto:"mascara con filtro",
-    marca:"stim",
-    modelo:"bifiltro",
-    color:"azul",}]
+                    <td >${prod.cantidad}</td>
 
-console.log (proteccionDeAltura); 
-console.log (proteccionOcular);
-console.log (proteccionRespiratoria);
+                    <td>$${prod.precio}</td>` 
+                contenedorCarrito.appendChild(Nuevoelemento);
+             });     
+    } 
+}
+sumarPrecios()
+function sumarPrecios(){
+    let total = 0;
+    const precioHTML = document.getElementById("totalPrecio");
+    carrito.forEach(prod => {
+        const precio = Number(prod.precio);
+        const cantidad = Number(prod.cantidad);
+        return total = total + precio * cantidad
+    })
+precioHTML.innerHTML = `<button class="btn btn-primary button-checkout">Pagar $${total.toFixed(2)}</button>`
+    $(".btn-primary").click(function() {
+        swal({
+            title: "Su pedido se ha registrado exitosamente",
+            icon:"success",
+        })        
+        carrito = [];        
+        sumarPrecios()
+        renderCarrito() 
+})      
+    const miBoton = document.createElement('button');
+    miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+    miBoton.textContent = 'Vaciar carrito';
+    precioHTML.appendChild(miBoton);
+    miBoton.addEventListener('click',VaciarCarrito)
+
+        function VaciarCarrito() {
+            Swal.fire({
+                title: "Seguro que desea vaciar el carrito?",
+                text: "Una vez confirmado no puede deshacerse",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => { 
+                if (willDelete) {
+                    carrito = [];        
+                    sumarPrecios()
+                    renderCarrito() 
+                } 
+                else {
+                    Swal.fire("Su carrito se encuentra protegido, continue comprando");
+                }
+            });
+        }
+}
 
 
 
